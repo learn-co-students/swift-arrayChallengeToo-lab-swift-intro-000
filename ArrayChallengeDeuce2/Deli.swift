0020ee48c98573af38257aadd19e30135516f1e2
briecoyle <brie.coyle@gmail.com>
2016-10-06 16:38:48 -0400

    Array Challenge Deuce Lab completed.

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,47 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
-        
+        if (name == "Billy Crystal") {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else if (name == "Meg Ryan") {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if (line[0] == name) {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
     
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        if (line.count == 0) {
+            return "There is no one to be served."
+        } else {
+            let served = line[0]
+            line.remove(at: 0)
+            return "Now serving \(served)!"
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        var description: [String] = []
+        if (line.count == 0) {
+            return "The line is currently empty."
+        } else {
+            for (index, name) in line.enumerated() {
+                description.append("\(index + 1). \(name)")
+            }
+        }
+        return "The line is:\n\(description.joined(separator: "\n"))"
     }
-    
+
 }
+
