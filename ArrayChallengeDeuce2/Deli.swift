7e09b67c5a74e174f83b3ac1a5491f37bcad2f93
jajuki13 <khkim1776@gmail.com>
2016-10-12 23:42:42 -0400

    need to fix few string issues and proper arrayItem being called

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -67,3 +67,75 @@ class Deli {
     - for in loop for the line description
     - need to also pull that position out for the addNameToLine func
 */
+
+/*
+ var line: [String] = []
+ var phrase = ""
+ 
+ func addNameToLine(name: String) -> String {
+ switch name {
+ case "Billy Crystal":
+ line.insert(name, at: 0)
+ phrase = "Welcome Billy Crystal! You can sit wherever you like."
+ case "Meg Ryan":
+ line.insert(name, at: 0)
+ phrase = "Welcome Meg Ryan! You can sit wherever you like."
+ default:
+ line.append(name)
+ phrase = "Welcome \(name), you're number \(line) in line" // need to find the position of this persons line and interpolate it in line.
+ }
+ print("\(line)")
+ print(phrase)
+ return phrase
+ }
+ 
+ addNameToLine(name: "James")
+ addNameToLine(name: "John")
+ addNameToLine(name: "Meg Ryan")
+ addNameToLine(name: "Billy Crystal")
+ addNameToLine(name: "Meg")
+ 
+ //=================================================
+ 
+ func nowServing() -> String {
+ var shouts = ""
+ var onLine = ""
+ if !line.isEmpty {
+ onLine = line.first!
+ print("Now serving \(onLine)!")
+ shouts = "Now serving \(onLine)!"
+ line.removeFirst()
+ } else {
+ shouts = "There is no one to be served"
+ }
+ print(line)
+ return shouts
+ }
+ 
+ nowServing()
+ nowServing()
+ 
+ //=================================================
+ func lineDescription() -> String {
+ var currentLineStatus = ""
+ if !line.isEmpty {
+ for (index, person) in line.enumerated() {
+ print("\(index + 1). \(person)\n")
+ }
+ } else {
+ currentLineStatus = "The line is currently empty."
+ }
+ 
+ return currentLineStatus
+ }
+ 
+ lineDescription()
+ lineDescription()
+ 
+ /*
+ TODO:
+ - "The line is:\n1. Rob\n2. Catherine\n3. Paul\n4. Dom" needs to be added.
+ - "Welcome Name, you're number # in line" the number of place needs to be added, right now it's an array.
+ -
+ */
+ */
