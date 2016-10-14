e7b38bf295de3c460a9b664efac46c9fd327fad4
ac <ac@Jaxxs-MBP.home>
2016-10-14 10:24:13 -0400

    completed lab challenge

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,24 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        var msg = String()
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            
+            msg = "Welcome \(name)! You can sit wherever you like."
+            line.insert(name, at: 0)
+        }
+        else {
+            if line.isEmpty {
+                msg = "Welcome \(name), you're first in line!"
+            }
+            else {
+                msg = "Welcome \(name), you're number \(line.count+1) in line."
+            }
+            line.append(name)
+
+        }
         
+        return msg
     }
     
     
@@ -25,7 +42,14 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        var servingMsg = String()
+        if line.isEmpty {
+            servingMsg = "There is no one to be served."
+        }
+        else {
+            servingMsg = "Now serving \(line.removeFirst())!"
+        }
+        return servingMsg
     }
     
     
@@ -33,7 +57,19 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        var lineDespMsg:String
+        if line.isEmpty {
+            lineDespMsg = "The line is currently empty."
+        }
+        else {
+            lineDespMsg = "The line is:"
+            for (idx,val) in line.enumerated() {
+                lineDespMsg.append("\n\(idx+1). \(val)")
+            }
+        }
+        return lineDespMsg
     }
+    
+    
     
 }
