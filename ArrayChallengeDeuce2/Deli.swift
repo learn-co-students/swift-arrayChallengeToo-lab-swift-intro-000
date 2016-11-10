9d6060410f5264e99537231b030cf54ebc178363
MelissaGarrett <melissa_garrett@hotmail.com>
2016-11-10 16:26:36 -0600

    Lab changes

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,25 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        if (name == "Billy Crystal" || name == "Meg Ryan") {
+            line.insert(name, at: 0)
+        } else {
+            line.append(name)
+        }
         
+        switch name {
+        case "Billy Crystal":
+            return ("Welcome \(name)! You can sit wherever you like.")
+        case "Meg Ryan":
+            return ("Welcome \(name)! You can sit wherever you like.")
+        default:
+            let index = line.index(of: name)
+            if (index == 0) {
+                return ("Welcome \(name), you're first in line!")
+            } else {
+                return ("Welcome \(name), you're number \(index!+1) in line.")
+            }
+        }
     }
     
     
@@ -25,7 +43,13 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        if (line.isEmpty) {
+            return("There is no one to be served.")
+        } else {
+            let currentCust = line[0]
+            line.remove(at: 0)
+            return("Now serving \(currentCust)!")
+        }
     }
     
     
@@ -33,7 +57,15 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        if (line.isEmpty) {
+            return("The line is currently empty.")
+        } else {
+            var custDesc = "The line is:"
+            for (index, step) in line.enumerated() {
+                custDesc += "\n\(index+1). \(step)"
+            }
+            return("\(custDesc)")
+        }
     }
     
 }
