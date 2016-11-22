4d1eedde4c85b44d34e53d95f9d0631192f61142
IT Development <itdevelopment@ITs-Mac-mini.local>
2016-11-22 14:24:04 +0000

    Lab Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,64 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
+        var greeting: String = ""
                 
        // TODO: Implement this function.
+        if name == "Billy Crystal" || name == "Meg Ryan"
+        {
+            line.insert(name, at: 0)
+            
+            greeting = "Welcome \(name)! You can sit wherever you like."
+        }
+        else
+        {
+            line.append(name)
+            
+            if line.count == 1 {
+                greeting = "Welcome \(name), you're first in line!"
+            }
+            else {
+                greeting = "Welcome \(name), you're number \(line.count) in line."
+            }
+            
+        }
         
+        return greeting
     }
     
     
     // 2
     func nowServing() -> String {
+        var currentlyServing: String = ""
         
         // TODO: Implement this function.
+        if !line.isEmpty
+        {
+            currentlyServing = "Now serving \(line[0])!"
+            
+            line.remove(at: 0)
+        } else {
+            currentlyServing = "There is no one to be served."
+        }
 
+        return currentlyServing
     }
     
     
     // 3
     func lineDescription() -> String {
+        var theLineIs: String = "The line is:"
         
         // TODO: Implement this function.
-
+        if !line.isEmpty {
+            for (lineIndex, customer) in line.enumerated() {
+                theLineIs += "\n\(lineIndex + 1). \(customer)"
+            }
+        } else {
+            theLineIs = "The line is currently empty."
+        }
+    
+        return theLineIs
     }
     
 }
