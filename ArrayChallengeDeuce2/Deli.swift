f1db87a6e3d4cccbe265499d7c822ffd9bd977c5
Raquel Rahmey <rrahmey@aol.com>
2016-12-06 14:56:21 -0500

    completed problems

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -8,6 +8,8 @@
 
 
 
+import UIKit
+
 
 class Deli {
     
@@ -15,25 +17,56 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert(name, at:0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        default:
+            line.append(name)
+            switch line.count {
+            case 1:
+                return "Welcome \(name), you're first in line!"
+            default:
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
-    
-    
+
+
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
-    }
+        switch line.count {
+        case 0:
+            return "There is no one to be served."
+        default:
+            let customer = line.remove(at: 0)
+            return "Now serving \(customer)!"
+        }
+        }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        switch line.count {
+        case 0:
+            return "The line is currently empty."
+        default:
+            var result = "The line is:\n"
+            for (index, name) in line.enumerated() {
+                switch index {
+                case line.count - 1:
+                    result.append("\(index+1). \(name)")
+                default:
+                    result.append("\(index+1). \(name)\n")
+                }
+            }
+            return result
+        }
     }
     
 }
