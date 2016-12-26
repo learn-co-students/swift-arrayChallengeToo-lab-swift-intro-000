779f37ac50d234bf2bd9ccc5a55990d524a6ac62
Autumn Raine <autumnraine@Ambers-MBP.home>
2016-12-25 20:36:32 -0500

    Lab Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,24 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
-        
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like"
+        case "Meg Whitman":
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like"
+        default:
+            line.append(name)
+            switch line.count {
+            case 1:
+                return "Welcome \(name), you are first in line"
+            default:
+                return "Welcome \(name), you are \(line.count) in line"
+                
+            }
+            
+        }
     }
     
     
@@ -25,6 +42,13 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
+        switch line.count {
+        case 0:
+            return "There is no one in line"
+        default:
+            let customer = line.remove(at: 0)
+            return "Now serving \(customer)!"
+        }
 
     }
     
@@ -33,7 +57,22 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
+        switch line.count {
+        case 0:
+            return "The line is currently empty"
+        default:
+            var result = "The line is:\n"
+            for (index, name) in line.enumerated() {
+                switch index {
+                case line.count - 1:
+                    result.append("\(index+1). \(name)")
+                default:
+                    result.append("\(index+1). \(name)")
+                }
+        }
+            return result
 
     }
     
+}
 }
