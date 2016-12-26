cc694931de33e16042114f7158099fb40018e553
JJJV <juan@jaramillo.net>
2016-12-26 00:15:58 -0600

    Deli Lab done
    watch your syntax, fucking anal retentive

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,57 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        }  else if line.count == 0 {
+            line.insert(name, at: 0)
+            return "Welcome \(name), you're first in line!"
+            
+        }  else {
+            line.append(name)
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
         
     }
     
     
     // 2
+    
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        if line.count == 0 {
+            return "There is no one to be served."
+        } else {
+            
+            let firstInLine: String = line[0]
+            line.remove(at:0)
+            return "Now serving \(firstInLine)!"
+            
+        }
+        
     }
-    
     
     // 3
-    func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
-    }
+    
+     func lineDescription() -> String {
+            
+            if line.count == 0 {
+                return "The line is currently empty."
+            } else {
+                
+                var waitingList: String = ""
+                for (index,customer) in line.enumerated() {
+                    
+                    waitingList = "\(waitingList)\n" + "\(index + 1). \(customer)"
+                }
+                
+               // print("The line is: \n \(waitingList)")
+                return  "The line is:\(waitingList)"
+            }
+            
+        }
     
 }
