5e78495e15ec35e378bc8d1dbfe2d23507a3ba4a
Jacque DesMarais <jacquedes@gmail.com>
2017-01-01 10:06:07 -0600

    solution
    solution to array duce

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -16,24 +16,46 @@ class Deli {
     // 1
     func addNameToLine(name: String) -> String {
                 
-       // TODO: Implement this function.
-        
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at:0)
+            return "Welcome \(name)! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        default:
+            line.append(name)
+            
+            if (line.count == 1) {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
+
     
-    
-    // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        if (line.isEmpty) {
+            return "There is no one to be served."
+        } else {
+            let served = line.remove(at: 0)
+            return "Now serving \(served)!"
+        }
     }
     
     
-    // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            var order = "The line is:"
+            for (index, name) in line.enumerated() {
+                order.append("\n\(index + 1). \(name)")
+            }
+            return order
+        }
     }
-    
 }
