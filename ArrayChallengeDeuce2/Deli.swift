345150c23597576f571c478f566874c7cf5824bd
4Ring <ukrainianslavic@gmail.com>
2017-01-07 02:43:29 -0800

    Completed Lab

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,57 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        var return_String: String
         
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            
+            line.insert(name, at: 0)
+            return_String = "Welcome \(name)! You can sit wherever you like."
+            
+        } else {
+            line.append(name)
+            if line.count == 1 {
+                return_String = "Welcome \(name), you're first in line!"
+            
+            } else {
+            return_String = "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
+        return return_String
     }
     
     
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        var return_String: String
+        
+        if line.isEmpty {
+            return_String = "There is no one to be served."
+        
+        } else {
+            return_String = "Now serving \(line[0])!"
+            line.remove(at: 0)
+        }
+        return return_String
     }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        var return_String: String
+        
+        if line.isEmpty {
+            return_String = "The line is currently empty."
+        
+        } else {
+            return_String = "The line is:"
+            for (index, person) in line.enumerated() {
+                return_String.append("\n\(index + 1). \(person)")
+            }
+        }
+        return return_String
     }
     
 }
