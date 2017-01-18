7a3b221f310128bf607d27ad09b3b375bd5a4051
Ryan Hennings <r.hennings11@gmail.com>
2017-01-17 23:53:26 -0800

    Array challenge 2 - Deli Queue

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -16,24 +16,49 @@ class Deli {
     // 1
     func addNameToLine(name: String) -> String {
                 
-       // TODO: Implement this function.
-        
+        switch name {
+        case "Billy Crystal":
+            line.insert("Billy Crystal", at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert("Meg Ryan", at: 0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        default:
+            if line.isEmpty {
+                line.append(name)
+                return "Welcome \(name), you're first in line!"
+            } else {
+                line.append(name)
+                return "Welcome \(name), you're number \(String(line.count)) in line."
+            }
+        }
     }
-    
     
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            return "Now serving \(line.removeFirst())!"
+        }
     }
-    
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        var lineList = "The line is:\n"
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            for (index, name) in line.enumerated() {
+                if index < line.count-1 {
+                    lineList += "\(index+1). \(name)\n"
+                } else {
+                    lineList += "\(index+1). \(name)"
+                }
+            }// end for loop
+            return lineList
+        }
     }
     
 }
