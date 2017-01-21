39ab9f30677d08aa66d7cc9ed38190b9ed6775a8
mac <mac@macs-iMac.local>
2017-01-21 12:26:29 +0200

    Lab Completed

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,16 +15,43 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
-        
+        var greeting : String
+
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at: 0)
+            greeting = "Welcome \(name)! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert(name, at: 0)
+            greeting = "Welcome \(name)! You can sit wherever you like."
+        default:
+            line.append(name)
+            if line.count == 1 {
+                greeting = "Welcome \(name), you're first in line!"
+            }
+            else {
+                greeting = "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
+        return greeting
     }
     
     
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
+        var serving : String
+        var name : String
+        
+        if line.count != 0 {
+            name = line[0]
+            line.remove(at: 0)
+            serving = "Now serving \(name)!"
+        }
+        else {
+            serving = "There is no one to be served."
+        }
+        return serving
 
     }
     
@@ -32,7 +59,20 @@ class Deli {
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
+        var lineDescription : String = ""
+        
+        if line.count == 0 {
+            lineDescription = "The line is currently empty."
+        }
+        else {
+            for (i, name) in line.enumerated() {
+                if i == 0 {
+                    lineDescription = "The line is:"
+                }
+                lineDescription = lineDescription + "\n\(i+1). \(name)"
+            }
+        }
+        return lineDescription
 
     }
     
