e956479340e39b67ec844a2980f65aff953fde11
Terrence <Terrence@Terrences-iMac.local>
2017-01-23 14:16:45 -0500

    Challenge2

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,65 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        let postion = line.count + 1
+        if (name == "Billy Crystal" || name == "Meg Ryan"){
+            line.insert(name, at: 0)
+        } else {
+            line.append(name)
+        }
         
+        switch name {
+            case "Billy Crystal":
+                return "Welcome Billy Crystal! You can sit wherever you like."
+            case "Meg Ryan":
+                return "Welcome Meg Ryan! You can sit wherever you like."
+            case line[0]:
+                return "Welcome \(name), you're first in line!"
+            default:
+                return "Welcome \(name), you're number \(postion) in line."
+        }
     }
     
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+            
+        } else {
+            
+            let firstInLine = line[0]
+            line.remove(at: 0)
+            return "Now serving \(firstInLine)!"
+            }
     }
-    
+    //helper function
+        
+        func buildList() -> [String]{
+            var customersEnum:[String] = []
+            
+            for (index,customersInLine) in line.enumerated(){
+                customersEnum.append("\n\(index + 1 ). \(customersInLine)")
+            }
+            return customersEnum
+        }
     
     // 3
     func lineDescription() -> String {
+       var lineFormat: String = ""
         
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "The line is currently empty."
+        
+        } else {
+        
+            for customers in buildList(){
+                lineFormat += "\(customers)"
+            }
+        return "The line is:\(lineFormat)"
+        }
     }
-    
 }
+
+
+
