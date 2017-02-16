f055120f3c026b33a1be37a31932b971b8044a22
Stephen Harrington <stephenharrington04@yahoo.com>
2017-02-16 11:17:07 -0600

    Completed Problems

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,60 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
-        
+            var greeting = String ()
+            switch name {
+            case "Billy Crystal":
+                line.insert("\(name)", at: 0)
+            case "Meg Ryan":
+                line.insert("\(name)", at: 0)
+            default:
+                line.append("\(name)")
+            }
+            switch line[0] {
+            case "Billy Crystal":
+                greeting = "Welcome Billy Crystal! You can sit wherever you like."
+            case "Meg Ryan":
+                greeting = "Welcome Meg Ryan! You can sit wherever you like."
+            default:
+                for (index, person) in line.enumerated() {
+                    if index == 0 {
+                        greeting = "Welcome \(person), you're first in line!"
+                    } else {
+                        greeting = "Welcome \(person), you're number \(index + 1) in line."
+                    }
+                }
+            }
+            return greeting
     }
     
-    
-    // 2
     func nowServing() -> String {
+        var youGotServed = String ()
+        if line.isEmpty {
+            youGotServed = "There is no one to be served."
+        } else {
+            youGotServed = "Now serving \(line[0])!"
+            line.remove(at: 0)
+        }
+        return youGotServed
         
-        // TODO: Implement this function.
-
     }
     
-    
-    // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        var lineStatus = String ()
+        if line.isEmpty {
+            lineStatus = "The line is currently empty."
+        } else {
+            lineStatus = "The line is:"
+            for (index, name) in line.enumerated(){
+                lineStatus.append("\n\(index+1). \(name)")
+            }
+        }
+        return lineStatus
     }
     
 }
+
+
+
+
+
