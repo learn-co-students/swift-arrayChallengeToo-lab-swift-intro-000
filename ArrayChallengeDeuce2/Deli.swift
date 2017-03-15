f0756c247b6cc1c12a50fbd3adfad42dfa1ea488
Morgan McCauley <morganmccauley@Morgans-MacBook-Pro.local>
2017-03-15 21:58:47 +1300

    array lab done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,8 +15,23 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        
+        var greeting: String
+        
+        if (name == "Billy Crystal" || name == "Meg Ryan" ) {
+            line.insert(name, at: 0)
+            greeting = "Welcome " + name + "! You can sit wherever you like."
+            
+        } else {
+            line.append(name)
+            greeting = "Welcome " + name + ", you're number " + "\(line.count)" + " in line."
+        }
+        
+        if (line.count == 1){
+            greeting = "Welcome " + name + ", you're first in line!"
+        }
+        
+        return greeting
         
     }
     
@@ -24,16 +39,39 @@ class Deli {
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        var serving: String
+        
+        if (line.count == 0){
+            serving = "There is no one to be served."
+        } else {
+            serving = "Now serving " + line[0] + "!"
+            line.remove(at: 0)
+        }
+        
+        return serving
     }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        var status: String
+        
+        if (line.count == 0){
+            status = "The line is currently empty."
+        } else {
+            status = "The line is:\n"
+            
+            for (index, person) in line.enumerated() {
+                status += "\(index + 1). " + person
+                
+                if (line.count - 1 != index) {
+                    status += "\n"
+                }
+            }
+        }
+        
+        return status
     }
     
 }
