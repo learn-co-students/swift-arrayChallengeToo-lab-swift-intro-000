a953df876672626fc2934a6f045a2f8d5bf159a6
RaymondAtivie <raymondativie@gmail.com>
2017-03-19 16:02:48 +0100

    ans

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,23 +17,71 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        if(name == "Meg Ryan" || name == "Billy Crystal"){
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        }else{
+            line.append(name)
+            let lc = line.count
+            
+            if lc == 1{
+                return "Welcome \(name), you're first in line!"
+            }else{
+                return "Welcome \(name), you're number \(lc) in line."
+            }
+        }
         
     }
     
     
     // 2
     func nowServing() -> String {
+        let lc = line.count
         
-        // TODO: Implement this function.
-
+        if lc == 0{
+            return "There is no one to be served."
+        }else{
+            let name = line[0]
+            line.remove(at: 0)
+            return "Now serving \(name)!"
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
+        let lc = line.count
         
-        // TODO: Implement this function.
-
+        if lc == 0{
+            return "The line is currently empty."
+        }else{
+            var listString = "The line is:"
+            
+            for (ix, name) in line.enumerated(){
+                listString.append("\n\(ix+1). \(name)")
+                print(listString)
+            }
+            
+            print(listString)
+            
+            return listString
+        }
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
