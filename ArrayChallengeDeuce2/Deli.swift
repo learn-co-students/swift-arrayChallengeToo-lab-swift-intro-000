2c143bd5bb9d09ddcdba6108fb40f668c14c42bf
Maksim Alekseichik <maximaleks@yandex.ru>
2017-04-10 11:34:05 +0300

    Array-2 lab finished

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,60 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+        var greeting = String()
+        
+        
+        switch name {
+        case "Billy Crystal":
+            line.insert("Billy Crystal", at: 0)
+            greeting = "Welcome Billy Crystal! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert("Meg Ryan", at: 0)
+            greeting = "Welcome Meg Ryan! You can sit wherever you like."
+        default:
+            line.append(name)
+            if line.count < 2 {
+                greeting = "Welcome \(name), you're first in line!"
+            }
+            else {
+                greeting = "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
+        
+        return greeting
     }
     
     
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        var info = String()
+        
+        if line.isEmpty {
+            info = "There is no one to be served."
+        } else {
+            if line.count > 0 {
+                info = "Now serving \(line[0])!"
+                line.remove(at: 0)
+            }
+        }
+        
+        return info
     }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        var info = String()
+        
+        if line.isEmpty {
+            info = "The line is currently empty."
+        } else {
+            info = "The line is:\n1. \(line[0])\n2. \(line[1])\n3. \(line[2])\n4. \(line[3])"
+        }
+        return info
     }
     
 }
