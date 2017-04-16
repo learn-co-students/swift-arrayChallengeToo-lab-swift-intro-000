cd1f3ffb684b062619c13079b1a1c515f293b9cd
Maneesh <maneesh.yadav@ahold.com>
2017-04-16 22:52:18 +0200

    Array Challenge Deuce Lab

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,6 +17,17 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if name == line[0] {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
         
     }
     
@@ -25,6 +36,14 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
+        
+        if line.count == 0 {
+            return "There is no one to be served."
+        } else {
+            let str = line[0]
+            line.remove(at: 0)
+            return "Now serving \(str)!"
+        }
 
     }
     
@@ -33,7 +52,18 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        if line.count == 0 {
+            return "The line is currently empty."
+        } else {
+            var str : String = ""
+            
+            for (index,name) in line.enumerated() {
+                str.append("\n\(index+1). \(name)")
+            }
+            
+            return "The line is:\(str)"
+        }
+        
     }
     
 }
