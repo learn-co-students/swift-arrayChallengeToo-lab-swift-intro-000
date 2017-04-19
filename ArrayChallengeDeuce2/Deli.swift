0a970e6141be3673a397d21c53b3511cc5051362
brianpoole <game.programmer@gmail.com>
2017-04-18 19:11:34 -0700

    solved

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,24 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+        var is_special = false
+        if name == "Billy Crystal" || name == "Meg Ryan"{
+            line.insert(name, at: 0)
+            is_special = true
+        }
+        else{
+            line.append(name)
+        }
         
+        var r_str = "Welcome \(name), you're number \(line.count) in line."
+        if(line.count == 1){
+            r_str = "Welcome \(name), you're first in line!"
+        }
+        else if(is_special){
+            r_str = "Welcome \(name)! You can sit wherever you like."
+        }
+        
+        return r_str
     }
     
     
@@ -25,7 +42,13 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        var r_str = "There is no one to be served."
+        if(line.count > 0){
+            r_str = "Now serving \(line[0])!"
+            line.remove(at: 0)
+        }
+        
+        return r_str
     }
     
     
@@ -33,6 +56,15 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
+        var r_str = "The line is currently empty."
+        if(line.count > 0){
+            r_str = "The line is:"
+            for (i, name) in line.enumerated(){
+                r_str += "\n\(i + 1). \(name)"
+            }
+        }
+        
+        return r_str
 
     }
     
