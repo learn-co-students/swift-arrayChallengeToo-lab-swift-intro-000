0251e4f6d6277d1a36f2b27739d220a4614d5692
astruckm <astruckm@yahoo.com>
2017-05-01 20:02:35 -0400

    2nd Array Challenge Lab

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,55 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
-        
+        if name == "Billy Crystal" {
+            line.insert(name, at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        } else if name == "Meg Ryan" {
+            line.insert(name, at: 0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if line.count == 1 {
+                return "Welcome \(name), you're first in line!" }
+            else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
     
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            let firstInLine = line[0]
+            line.remove(at: 0)
+            return "Now serving \(firstInLine)!"
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        var lineList: String = ""
+        var loopCount = 0
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            for (index, person) in line.enumerated() {
+                lineList += "\(index+1). \(person)"
+                print(lineList)
+                loopCount += 1
+                print(loopCount)
+                if loopCount < line.count {
+                    lineList += "\n"
+                }
+                print(lineList)
+            }
+            return "The line is:\n\(lineList)"
+        }
     }
-    
+
+    let test = "The line is:\n1. Rob\n2. Catherine\n3. Paul\n4. Dom"
 }
