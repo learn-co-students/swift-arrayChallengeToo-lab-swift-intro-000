0063d9faa72e91b229b6ff793942e35c084dde35
gasganno <jarrod@gasganno.com>
2017-05-02 17:00:54 -0400

    Array Challenge Deuce Lab Completed

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,17 +15,41 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+       // TODO: Implement this function.
+
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert(name, at: 0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        default:
+            line.append(name)
+            let position = line.index(of: name)!
+            if position == 0 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(position + 1) in line."
+            }
+        }
     }
-    
-    
+
+
     // 2
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        
+        if !line.isEmpty {
+            let person = line[0]
+            line.remove(at: 0)
+            return "Now serving \(person)!"
+        } else {
+            return "There is no one to be served."
+        }
+        
     }
     
     
@@ -33,7 +57,17 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        
+        if !line.isEmpty {
+            var list: String = "The line is:"
+            for (index, person) in line.enumerated() {
+                let listMember = ("\n" + "\(index + 1). \(person)")
+                list.append(listMember)
+            }
+            return(list)
+        } else {
+            return "The line is currently empty."
+        }
     }
-    
+
 }
