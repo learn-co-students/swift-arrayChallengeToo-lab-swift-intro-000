8aec8a3f6dff1c3131916e1a6f269fecb7ce736e
felixvdl <vandelanottefelix@gmail.com>
2017-05-19 16:38:49 -0400

    completed

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,38 +15,30 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-        if name == "Billy Crystal" {
+        if name == "Billy Crystal" || name == "Meg Ryan" {
             line.insert(name, at: 0)
-            return "Welcome Billy Crystal! You can sit wherever you like."
-        } else if name == "Meg Ryan" {
-            line.insert(name, at: 0)
-            return "Welcome Meg Ryan! You can sit wherever you like."
-        } else {
-            line.append(name)
-            if line.count == 1 {
-                return "Welcome \(name), you're first in line!"
-            } else {
-                return "Welcome \(name), you're number \(line.count + 1) in line"
-            }
-        }
-       
-       // TODO: Implement this function.
+        } else { line.append(name)}
         
+        if name == "Meg Ryan" {
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        } else if name == "Billy Crystal" {
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        } else if line.index(of: name) == 0 {
+            return "Welcome \(name), you're first in line!"
+        } else {
+            return "Welcome \(name), you're number \(line.index(of: name)! + 1) in line."
+        }
     }
     
     
     // 2
     func nowServing() -> String {
+        
         if line.isEmpty {
             return "There is no one to be served."
         } else {
-            line.remove(at: 0)
-            return "Now serving \(line[0])!"
-            
+            return "Now serving \(line.remove(at: 0))!"
         }
-        
-        // TODO: Implement this function.
-
     }
     
     
@@ -57,13 +49,13 @@ class Deli {
             result = "The line is currently empty."
         } else {
             result = "The line is:"
-            for (index, customer) in line.enumerated() {
-                result.append("\(index) \(customer)")
+            for (index, name) in line.enumerated() {
+                result.append("\n\(index + 1). \(name)")
             }
         }
+        
         return result
-        // TODO: Implement this function.
-
+        
     }
     
 }
