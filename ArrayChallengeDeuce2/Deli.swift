960e35820a6d6e4f29b7f1705d40fc97ca2a6539
tamarkandathi <tamar.kandathi@gmail.com>
2017-05-25 17:11:38 -0400

    arrayChallengeDeuce2 -done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,41 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        }
+        line.append(name)
+        if line.count == 1 {
+            return "Welcome \(name), you're first in line!"
+        }
+        return "Welcome \(name), you're number \(line.count) in line."
     }
     
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        if !line.isEmpty {
+            let customer = line[0]
+            line.remove(at: 0)
+            return "Now serving \(customer)!"
+        }
+        return "There is no one to be served."
     }
     
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        if !line.isEmpty {
+            var customerLine = "The line is:"
+            
+            for (index, name) in line.enumerated() {
+                customerLine.append("\n\(index + 1). \(name)")
+            }
+            return customerLine
+        }
+        return "The line is currently empty."
     }
     
 }
