b0219cf7f3a67096a34ba0af9f99b734197123c3
ayamouallem <aya_mouallem@hotmail.com>
2017-06-03 13:18:04 +0300

    Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -16,7 +16,26 @@ class Deli {
     // 1
     func addNameToLine(name: String) -> String {
                 
-       // TODO: Implement this function.
+        if name=="Meg Ryan" || name=="Billy Crystal"
+        {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        }
+
+        else if line.isEmpty==true
+        {
+            line.append(name)
+            return "Welcome \(name), you're first in line!"
+        }
+        else
+        {
+            line.append(name)
+            var nb=0
+            for (index, name) in line.enumerated(){
+                 nb=index+1
+            }
+            return "Welcome \(name), you're number \(nb) in line."
+        }
         
     }
     
@@ -24,7 +43,15 @@ class Deli {
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
+        if line.isEmpty==true{
+            return "There is no one to be served."
+        }
+        else{
+            var name=line[0]
+            line.remove(at: 0)
+            return "Now serving \(name)!"
+            
+        }
 
     }
     
@@ -32,8 +59,18 @@ class Deli {
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
+        if line.isEmpty{
+            return "The line is currently empty."
+        }
+        else{
+            print("The line is:")
+            for name in line
+            {
+                print("line.index(of:name). \(name) \n")
+            }
+            return "The line is:\n1. Rob\n2. Catherine\n3. Paul\n4. Dom"
+        }
 
-    }
     
+}
 }
