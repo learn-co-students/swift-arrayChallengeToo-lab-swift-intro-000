dd2d66f8718569b6ea6d0ff6c13b3e71d47c2065
JakeCormier <jake.r.cormier@gmail.com>
2017-07-24 15:48:08 -0600

    Complete problems -repo fix

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,9 +15,19 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+        // TODO: Implement this function.
+        if name == "Meg Ryan" || name == "Billy Crystal" {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if line.count == 1 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
     
     
@@ -25,7 +35,17 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        var serve = ""
+        
+        if line.isEmpty {
+            serve = "There is no one to be served."
+        } else {
+            serve =  "Now serving \(line[0])!"
+            line.remove(at: 0)
+        }
+        
+        return serve
+        
     }
     
     
@@ -33,7 +53,21 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        var description = ""
+        
+        if line.isEmpty {
+            description = "The line is currently empty."
+        } else {
+            description = "The line is:"
+            for (index, item) in line.enumerated() {
+            description += "\n\(index+1). \(item)"
+            }
+        }
+    
+        return description
+        
+        
     }
     
 }
+
