b0a786edbc98fffbf3c108da315abdcae869f7eb
wc1404 <wc1404@nyu.edu>
2017-08-04 12:27:03 -0400

    Lab Done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -6,34 +6,49 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
-
-
-
 class Deli {
     
     var line: [String] = []
     
     // 1
     func addNameToLine(name: String) -> String {
-                
        // TODO: Implement this function.
-        
+        if name == "Billy Crystal" || name == "Meg Ryan"{
+            line.insert(name,at:0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if line[0] == name {
+                return "Welcome \(name), you're first in line!"
+            }
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
     }
     
     
     // 2
     func nowServing() -> String {
-        
         // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            let serving: String = line.remove(at: 0)
+            return "Now serving \(serving)!"
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
-        
         // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            var lineDescription = "The line is:"
+            for (place, person) in line.enumerated() {
+                lineDescription += "\n\(place + 1). \(person)"
+            }
+            return lineDescription
+        }
     }
-    
 }
