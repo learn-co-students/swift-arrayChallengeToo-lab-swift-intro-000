b6717df2c98df1bc39b9a73bad6c0f9c27ef2672
stephanos <stephanos@goldmail.etsu.edu>
2017-09-22 09:08:20 -0700

    Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,17 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
-        
+        if name == "Meg Ryan" || name == "Billy Crystal" {
+            line.insert(name, at: 0)
+            return "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            if line.count == 1 {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
     
     
@@ -25,15 +35,28 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        if line.count == 0 {
+            return "There is no one to be served."
+        } else {
+            let customer = line[0]
+            line.remove(at: 0)
+            return "Now serving \(customer)!"
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
+        var currentLine = "The line is:"
         
         // TODO: Implement this function.
-
+        if line.count == 0 {
+            return "The line is currently empty."
+        }
+            for(index, name) in line.enumerated() {
+                currentLine += "\n\(index + 1). \(name)"
+            }
+        return currentLine
     }
     
 }
