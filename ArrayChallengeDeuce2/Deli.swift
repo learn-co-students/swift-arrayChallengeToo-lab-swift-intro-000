c5f4b21bed8d203fb18970e87ac0fea356b9bf35
Yue Feng <strangedigital@gmail.com>
2017-09-28 23:10:40 -0400

    done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,24 +15,49 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        var returnmessage:String
+        switch name {
+            case "Billy Crystal", "Meg Ryan":
+                returnmessage = "Welcome \(name)! You can sit wherever you like."
+                line.insert(name,at:0)
+            default:
+                if line.isEmpty {
+                    returnmessage = "Welcome \(name), you're first in line!"
+                } else {
+                    returnmessage = "Welcome \(name), you're number \(line.count+1) in line."
+                }
+                line.append(name)
+        }
+       return returnmessage
         
     }
     
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        var returnmessage:String
+        if (line.isEmpty) {
+            returnmessage = "There is no one to be served."
+        } else {
+            returnmessage = "Now serving \(line[0])!"
+            line.removeFirst()
+        }
+        return returnmessage
     }
     
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
+        var returnmessage:String
+        if (line.isEmpty) {
+            returnmessage = "The line is currently empty."
+        } else {
+            returnmessage = "The line is:"
+            for (index,person) in line.enumerated() {
+                returnmessage.append("\n\(index+1). \(person)")
+            }
+        }
+        return returnmessage
 
     }
     
