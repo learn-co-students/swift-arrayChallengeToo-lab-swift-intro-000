18ea029f5cbbf4b6a68c0bcf1ca8fe115f0a2dfd
LisaMartil <martiny.lisa96@googlemail.com>
2017-12-01 11:30:23 +0100

    Done

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -12,12 +12,33 @@
 class Deli {
     
     var line: [String] = []
+    var vip = ["Billy Crystal", "Meg Ryan"]
+    var position = 0
     
     // 1
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
         
+        if (vip.contains(name)) {
+        line.insert(name, at:0)
+        } else {
+            line.append(name)
+        }
+        
+        
+        if (vip.contains(name)) {
+            return "Welcome \(name)! You can sit wherever you like."
+        } else if (line[0] == name) {
+            return "Welcome \(name), you're first in line!"
+        } else {
+            for (i, x) in line.enumerated() {
+                if (x == name) {
+                position = i+1
+                }
+            }
+            return "Welcome \(name), you're number \(position) in line."
+        }
     }
     
     
@@ -25,7 +46,13 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        if (line.isEmpty) {
+            return "There is no one to be served."
+        } else {
+            var s = line[0]
+            line.remove(at: 0)
+            return "Now serving \(s)!"
+        }
     }
     
     
@@ -33,7 +60,14 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        if (line.isEmpty) {
+            return "The line is currently empty."
+        } else {
+            var s : String = "The line is:"
+            for (i, x) in line.enumerated() {
+                s = s + "\n\(i+1). \(x)"
+            }
+        return s
+        }
     }
-    
 }
