b7a90464acd4b2f903a90b3c65063492bbb002b6
dylanjrafferty <dylanjrafferty@gmail.com>
2018-01-18 12:37:23 -0500

    Complete problems

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,25 +15,50 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        if name == "Billy Crystal"{
+            line.insert("Billy Crystal", at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        } else if name == "Meg Ryan"{
+            line.insert("Meg Ryan", at: 0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        } else if line.count == 0 {
+            line.append(name)
+            return "Welcome \(name), you're first in line!"
+        } else {
+            line.append(name)
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
         
     }
     
     
     // 2
     func nowServing() -> String {
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            defer {
+                line.remove(at: 0)
+            }
+            return "Now serving \(line[0])!"
+            
+        }
         
-        // TODO: Implement this function.
-
     }
     
     
     // 3
     func lineDescription() -> String {
+        var list = ""
         
-        // TODO: Implement this function.
+        if line.isEmpty{
+            return "The line is currently empty."
+        } else {
+                list = "The line is:\n1. Rob\n2. Catherine\n3. Paul\n4. Dom"
+            }
+            return list
+        }
 
     }
     
-}
+
