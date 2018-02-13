3233db0ba6c4254320808fcb9b62287a7406128d
nikandv <nikandrov.ant@ya.ru>
2018-02-13 15:08:24 +0300

    Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,7 +17,18 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
-        
+        if (line.count==0) {
+            line.append(name)
+            return "Welcome \(name), you're first in line!"
+        } else {
+            if (name=="Billy Crystal" || name=="Meg Ryan") {
+                line.insert(name, at: 0)
+                return "Welcome \(name)! You can sit wherever you like."
+            } else {
+                line.append(name)
+                return "Welcome \(name), you're number \(line.count) in line."
+            }
+        }
     }
     
     
@@ -25,7 +36,13 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        if (line.count == 0) {
+            return "There is no one to be served."
+        } else {
+            let result = "Now serving \(line[0])!"
+            line.remove(at: 0)
+            return result
+        }
     }
     
     
@@ -33,7 +50,15 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
+        if (line.count==0) {
+            return "The line is currently empty."
+        } else {
+            var result = "The line is:"
+            for (index, person) in line.enumerated() {
+                result.append("\n\(index+1). \(person)")
+            }
+            return result
+        }
     }
     
 }
