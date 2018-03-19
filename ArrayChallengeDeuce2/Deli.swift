29afa981dba4121f43162b947d0692adc87d210c
Stanley Sakai <sakai.stanley@gmail.com>
2018-03-19 14:24:40 -0400

    Array Challenge Deuce Lab

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,24 +15,60 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        var greeting: String = ""
+        
+        if name == "Billy Crystal"  || name == "Meg Ryan" {
+            line.insert(name, at:  0)
+            greeting = "Welcome \(name)! You can sit wherever you like."
+        } else {
+            line.append(name)
+            for (i, guest) in line.enumerated() {
+                if i == 0 {
+                    greeting = "Welcome \(guest), you're first in line!"
+                } else {
+                    greeting = "Welcome \(guest), you're number \(i + 1) in line."
+                }
+            }
+        }
+        
+        return greeting
         
     }
     
     
     // 2
     func nowServing() -> String {
+        var greeting: String = ""
         
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            greeting = "There is no one to be served."
+        } else {
+            let currentGuest = line[0]
+            greeting = "Now serving \(currentGuest)!"
+            line.remove(at: 0)
+        }
+        
+        return greeting
+        
     }
     
     
     // 3
     func lineDescription() -> String {
+        var message: String = "The line is:"
         
-        // TODO: Implement this function.
+        if line.isEmpty{
+             message = "The line is currently empty."
+        } else {
+            var names: String = ""
+            
+            for (index, name) in line.enumerated() {
+                names = "\(index + 1). \(name)"
+                message += "\n\(names)"
+            }
+        }
+        
+        return message
 
     }
     
