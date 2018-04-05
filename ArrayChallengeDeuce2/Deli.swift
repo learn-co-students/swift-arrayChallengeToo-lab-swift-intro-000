282b409969d8a68eb19fdf66a3aca7775b7f8959
MlleDolce <andreakchristie@gmail.com>
2018-04-04 22:26:30 -0400

    Completed 'ArrayChallengeToo' Lab.  All tests pass.

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -7,33 +7,55 @@
 //
 
 
-
-
 class Deli {
     
     var line: [String] = []
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
         
+        if name == "Billy Crystal" || name == "Meg Ryan" {
+            line.insert(name, at: 0)
+        } else {
+            line.append(name)
+        }
+        print(line)
+        
+        if name == "Billy Crystal" {
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        } else if name == "Meg Ryan" {
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        } else if line.index(of: name) == 0 {
+            return "Welcome \(name), you're first in line!"
+        } else {
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
     }
-    
     
     // 2
     func nowServing() -> String {
-        
-        // TODO: Implement this function.
-
+        if !line.isEmpty {
+            let personAtFront = line[0]
+            line.remove(at: 0)
+            return "Now serving \(personAtFront)!"
+        } else {
+            return "There is no one to be served."
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        var peopleInLine: String = "The line is:"
+        
+        if !line.isEmpty {
+            for (index, person) in line.enumerated() {
+                peopleInLine.append("\n\(index + 1). \(person)")
+            }
+            return peopleInLine
+        } else {
+            return "The line is currently empty."
+        }
     }
-    
 }
