3cf40ae7ef604974e2a4ff8b69181f8b95adb0dd
winzardv11 <ridzwan.midris@gmail.com>
2018-04-24 23:48:00 +0800

    Local test no longer works. Help me

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,8 +15,24 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+      
+        switch name {
+        case "Billy Crystal":
+            line.insert(name, at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        case "Meg Ryan":
+            line.insert(name, at: 0)
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        default:
+            line.append(name)
+        }
+
+        switch line.count {
+        case 1:
+            return "Welcome \(name), you're first in line!"
+        default:
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
         
     }
     
@@ -25,7 +41,16 @@ class Deli {
     func nowServing() -> String {
         
         // TODO: Implement this function.
-
+        
+        switch line.count {
+        case 0:
+            return "There is no one to be served."
+        default:
+            let x = line[0]
+            line.remove(at: 0)
+            return "Now serving \(x)!"
+        }
+        
     }
     
     
@@ -33,7 +58,22 @@ class Deli {
     func lineDescription() -> String {
         
         // TODO: Implement this function.
-
-    }
-    
+        
+        switch line.isEmpty {
+        case true:
+            return "The line is currently empty."
+        default:
+            var x = "The line is:\n"
+            for (y, z) in line.enumerated(){
+                switch y {
+                case line.count - 1:
+                    x.append("\(y + 1). \(z)")
+                default:
+                    x.append("\(y + 1). \(z)\n")
+                }
+            }
+     return x
+}   
+       
+}
 }
