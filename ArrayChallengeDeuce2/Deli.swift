6ef02176ffec57c7ca8cf74c3d19326808464341
giddix23 <giddixcompany@gmail.com>
2018-06-07 22:17:33 -0400

    ArrayChallengeToo

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -15,8 +15,27 @@ class Deli {
     
     // 1
     func addNameToLine(name: String) -> String {
-                
-       // TODO: Implement this function.
+        
+        
+        
+        if name == "Billy Crystal"{
+            line.insert(name, at: 0)
+            return "Welcome Billy Crystal! You can sit wherever you like."
+           
+        } else if name == "Meg Ryan"{
+            line.insert(name, at: 0)
+            return("Welcome Meg Ryan! You can sit wherever you like.")
+        } else {
+            line.append(name)
+            if line[0] == name {
+                return "Welcome \(name), you're first in line!"
+            } else {
+                let y = line.count
+                return ("Welcome \(name), you're number \(y) in line.")
+            }
+        }
+        
+        
         
     }
     
@@ -24,16 +43,27 @@ class Deli {
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        if line.isEmpty {
+            return "There is no one to be served."
+        } else {
+            let firstInLine = line[0]
+            line.remove(at: 0)
+            return ("Now serving \(firstInLine)!")
+        }
     }
     
     
     // 3
     func lineDescription() -> String {
-        
-        // TODO: Implement this function.
-
+        var lineList: String = "The line is:"
+        if line.isEmpty {
+            return "The line is currently empty."
+        } else {
+            for (index, person) in line.enumerated(){
+                lineList.append("\n\(index + 1). \(person)")
+            }
+            print(lineList)
+            return lineList
+        }
     }
-    
 }
